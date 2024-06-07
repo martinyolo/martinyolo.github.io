e65636e21ca00e32e7ec34a8f4b7646500409c12
MartinBraaten <martin.isaksen@ad.eyeshare.no>
2024-06-07 11:26:42 +0200

    Update readme

--- lunsj/src/pages/PageNotFound.tsx
+++ lunsj/src/pages/PageNotFound.tsx
@@ -1,9 +1,17 @@
+import Button from "@mui/material/Button";
+import { useNavigate } from "react-router-dom";
+
 const PageNotFound = () => {
-    return (
-      <div>
-        <h1>404 - Page Not Found</h1>
-      </div>
-    );
-  };
-  
-  export default PageNotFound;
+  let navigate = useNavigate();
+
+  return (
+    <div>
+      <h1>404 - Page Not Found</h1>
+      <Button onClick={() => navigate("/")} variant="contained">
+        Tilbake til forsiden
+      </Button>
+    </div>
+  );
+};
+
+export default PageNotFound;

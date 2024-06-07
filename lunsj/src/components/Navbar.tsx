2f678e7a4bfca627b441fad0975b32c08357e956
MartinBraaten <martin.isaksen@ad.eyeshare.no>
2024-06-07 15:00:01 +0200

    update css and landingpage

--- lunsj/src/components/Navbar.tsx
+++ lunsj/src/components/Navbar.tsx
@@ -41,21 +41,20 @@ const Navbar = () => {
     };
 
     return (
-        <div id="c_section" className='fixed top-0 bg-white dark:bg-dark-lysGraa text-black w-screen h-auto content-center z-20 shadow-md'>
-            <div id="c_container" className='relative flex flex-col m-auto w-full max-w-7xl pt-5 px-10 justify-between'>
-                <div className='flex flex-row justify-between'>
-                {currentUser ? (
-                    <h1 className="text-4xl p-3 hover:cursor-pointer dark:text-dark-white">Stem i vei!</h1>
-                ) : (
-                    <h1 className="text-4xl p-3 hover:cursor-pointer dark:text-dark-white">Logg inn for å stemme</h1>
-                )}
-                    <div className='flex flex-col w-4/5'>
-                        <div className='flex flex-row justify-between'>
-
-                            <div className='flex flex-row gap-1 w-auto'>
+        <div>
+            <div>
+                <div>
+                    {currentUser ? (
+                        <h1>Stem i vei!</h1>
+                    ) : (
+                        <h1>Logg inn for å stemme</h1>
+                    )}
+                    <div>
+                        <div>
+                            <div style={{ position: 'fixed', top: '15px', right: '15px'}}>
                                 {currentUser ? (
                                     // if user is logged in, render logout button
-                                    <div className='flex flex-row gap-1 w-auto'>
+                                    <div>
                                         <Button disabled={disableButton} onClick={handleClickLogout} variant="contained">
                                             Logg ut
                                         </Button>
@@ -66,14 +65,13 @@ const Navbar = () => {
                                     </div>
                                 ) : (
                                     // if user is not logged in, render login button
-                                    <Button disabled={disableButton} onClick={handleClickLogin} variant="contained">Logg inn</Button>
+                                    <Button disabled={disableButton} onClick={handleClickLogin} variant="contained">Logg inn med Google</Button>
                                 )}
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-            <div className='h-1 bg-pu-gul'> </div>
         </div>
     );
 }
--- lunsj/src/pages/LandingPage.tsx
+++ lunsj/src/pages/LandingPage.tsx
@@ -1,43 +1,35 @@
-import React, { useEffect, useState } from "react";
-import axios from "axios";
+import React from "react";
+import RadioButtons from "../components/RadioButtons";
 
-const url1 = "https://widget.inisign.com/Widget/Customers/Customer.aspx?token=6e5cc038-e918-4f97-9a59-d2afa0456abf&scaleToFit=true"
-const url2 = "https://widget.inisign.com/Widget/Customers/Customer.aspx?token=a8923cdb-9d92-46bc-b6a4-d026c2cf9a89&scaleToFit=true"
-const url3 = "https://widget.inisign.com/Widget/Customers/Customer.aspx?token=756a5aa2-a95f-4d15-ad5a-59829741075b&scaleToFit=true"
-
-interface CustomURLProps {
-  url: string;
-  }
-
-const ExternalBodyContent = ({ url }: CustomURLProps) => {
-  const [bodyContent, setBodyContent] = useState(null);
-
-  useEffect(() => {
-    const fetchHtmlContent = async () => {
-      try {
-        const response = await axios.get(url);
-        const bodyStartIndex = response.data.indexOf("<body");
-        const bodyEndIndex = response.data.indexOf("</body>") + "</body>".length;
-        const bodyHTML = response.data.substring(bodyStartIndex, bodyEndIndex);
-        setBodyContent(bodyHTML);
-      } catch (error) {
-        console.error("Error fetching HTML content:", error);
-      }
-    };
-
-    fetchHtmlContent();
-  }, [url]);
-
-  return <div dangerouslySetInnerHTML={{ __html: bodyContent || "Loading..."  }} />;
-};
-  
+const url1 =
+  "https://widget.inisign.com/Widget/Customers/Customer.aspx?token=6e5cc038-e918-4f97-9a59-d2afa0456abf&scaleToFit=true";
+const url2 =
+  "https://widget.inisign.com/Widget/Customers/Customer.aspx?token=a8923cdb-9d92-46bc-b6a4-d026c2cf9a89&scaleToFit=true";
+const url3 =
+  "https://widget.inisign.com/Widget/Customers/Customer.aspx?token=756a5aa2-a95f-4d15-ad5a-59829741075b&scaleToFit=true";
 
 const LandingPage = () => {
   return (
-    <div style={{ display: "grid", gridTemplateColumns: "1fr 1fr 1fr", gap: "20px" }}>
-      <ExternalBodyContent url= {url1} />
-      <ExternalBodyContent url= {url2} />
-      <ExternalBodyContent url= {url3} />
+    <div style={{ height: "100vh", width: "100vw" }}>
+      <div style={{ display: "grid", gridTemplateColumns: "3fr 1fr", height: "100%" }}>
+        <div style={{ display: "grid", gridTemplateColumns: "1fr 1fr 1fr", height: "100%" }}>
+          <div style={{ margin: "5px", height: "100%", position: 'relative' }}>
+            <iframe src={url1} width="100%" height="100%" title="Page 1"></iframe>
+          </div>
+          <div style={{ margin: "5px", height: "100%", position: 'relative' }}>
+            <iframe src={url2} width="100%" height="100%" title="Page 2"></iframe>
+            <div style={{ position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', zIndex: 999 }}>
+              <RadioButtons />
+            </div>
+          </div>
+          <div style={{ margin: "5px", height: "100%", position: 'relative' }}>
+            <iframe src={url3} width="100%" height="100%" title="Page 3"></iframe>
+          </div>
+        </div>
+        <div style={{ position: 'relative' }}>
+          {/* statistikk her */}
+        </div>
+      </div>
     </div>
   );
 };

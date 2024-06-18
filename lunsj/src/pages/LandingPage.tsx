eebecfabf4e5fb1e8dda6ec792e4d5e9630b1362
MartinBraaten <martin.isaksen@ad.eyeshare.no>
2024-06-18 10:15:12 +0200

    remove radiobuttons

--- lunsj/src/pages/LandingPage.tsx
+++ lunsj/src/pages/LandingPage.tsx
@@ -18,9 +18,9 @@ const LandingPage = () => {
           </div>
           <div style={{ margin: "5px", height: "100%", position: 'relative' }}>
             <iframe src={url2} width="100%" height="100%" title="Page 2"></iframe>
-            <div style={{ position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', zIndex: 999 }}>
+{/*             <div style={{ position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', zIndex: 999 }}>
               <RadioButtons />
-            </div>
+            </div> */}
           </div>
           <div style={{ margin: "5px", height: "100%", position: 'relative' }}>
             <iframe src={url3} width="100%" height="100%" title="Page 3"></iframe>
